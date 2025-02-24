92becc5a1eeed20406fe5a2ab0f81b9a885fe33c
jason <jason@zgwit.com>
2025-02-25 06:26:43 +0800

    订阅数据

--- internal/mqtt.go
+++ internal/mqtt.go
@@ -3,28 +3,39 @@ package internal
 import (
 	"github.com/busy-cloud/boat/log"
 	"github.com/busy-cloud/boat/mqtt"
+	paho "github.com/eclipse/paho.mqtt.golang"
 	"strings"
 )
+
+const protocol = "modbus"
 
 func Startup() error {
 
 	//订阅数据
-	mqtt.Subscribe("link/base/+/up", func(topic string, payload []byte) {
-		ss := strings.Split(topic, "/")
-		id := ss[2]
-		gateway, err := EnsureGateway(id)
+	mqtt.Client.SubscribeMultiple(map[string]byte{
+		protocol + "/+/+/up": 0,
+		protocol + "/+/up":   0,
+	}, func(client paho.Client, message paho.Message) {
+		ss := strings.Split(message.Topic(), "/")
+		linker := ss[1]
+		incoming := ss[2]
+		gateway, err := EnsureGateway(linker, incoming)
 		if err != nil {
 			log.Error("gateway err:", err)
 			return
 		}
-		gateway.onData(payload)
+		gateway.onData(message.Payload())
 	})
 
 	//连接打开，加载设备
-	mqtt.Subscribe("link/base/+/open", func(topic string, payload []byte) {
-		ss := strings.Split(topic, "/")
-		id := ss[2]
-		gateway, err := EnsureGateway(id)
+	mqtt.Client.SubscribeMultiple(map[string]byte{
+		protocol + "/+/+/open": 0,
+		protocol + "/+/open":   0,
+	}, func(client paho.Client, message paho.Message) {
+		ss := strings.Split(message.Topic(), "/")
+		linker := ss[1]
+		incoming := ss[2]
+		gateway, err := EnsureGateway(linker, incoming)
 		if err != nil {
 			log.Error("gateway err:", err)
 			return
@@ -37,39 +48,47 @@ func Startup() error {
 	})
 
 	//关闭连接
-	mqtt.Subscribe("link/base/+/close", func(topic string, payload []byte) {
-		ss := strings.Split(topic, "/")
-		id := ss[2]
-		gateway := GetGateway(id)
+	mqtt.Client.SubscribeMultiple(map[string]byte{
+		protocol + "/+/+/close": 0,
+		protocol + "/+/close":   0,
+	}, func(client paho.Client, message paho.Message) {
+		ss := strings.Split(message.Topic(), "/")
+		linker := ss[1]
+		incoming := ss[2]
+		gateway, err := EnsureGateway(linker, incoming)
+		if err != nil {
+			log.Error("gateway err:", err)
+			return
+		}
 		if gateway != nil {
 			//gateway.Close()
 		}
 	})
 
-	//添加设备
-	mqtt.Subscribe("link/base/+/attach/+", func(topic string, payload []byte) {
-		ss := strings.Split(topic, "/")
-		id := ss[2]
-		id2 := ss[4]
-		gateway := GetGateway(id)
-		if gateway != nil {
-			err := gateway.LoadDevice(id2)
-			if err != nil {
-				log.Error("gateway err:", err)
-			}
-		}
-	})
-
-	//删除设备
-	mqtt.Subscribe("link/base/+/detach/+", func(topic string, payload []byte) {
-		ss := strings.Split(topic, "/")
-		id := ss[2]
-		id2 := ss[4]
-		gateway := GetGateway(id)
-		if gateway != nil {
-			gateway.UnLoadDevice(id2)
-		}
-	})
+	////添加设备
+	//mqtt.Subscribe("link/+/+/attach/+", func(topic string, payload []byte) {
+	//	ss := strings.Split(topic, "/")
+	//	id := ss[2]
+	//	id2 := ss[4]
+	//	gateway := GetGateway(id)
+	//	if gateway != nil {
+	//		err := gateway.LoadDevice(id2)
+	//		if err != nil {
+	//			log.Error("gateway err:", err)
+	//		}
+	//	}
+	//})
+	//
+	////删除设备
+	//mqtt.Subscribe("link/+/+/detach/+", func(topic string, payload []byte) {
+	//	ss := strings.Split(topic, "/")
+	//	id := ss[2]
+	//	id2 := ss[4]
+	//	gateway := GetGateway(id)
+	//	if gateway != nil {
+	//		gateway.UnLoadDevice(id2)
+	//	}
+	//})
 
 	return nil
 }
