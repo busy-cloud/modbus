ae19db7e645f63c86fbe200a3f46c6a25c949cf3
jason <jason@zgwit.com>
2025-03-31 21:21:26 +0800

    删除协议注册

--- modbus.go
+++ modbus.go
@@ -1,23 +1,5 @@
 package modbus
 
 import (
-	_ "embed"
-	"encoding/json"
-	"github.com/busy-cloud/boat/log"
-	"github.com/busy-cloud/iot/protocol"
 	_ "github.com/busy-cloud/modbus/internal"
 )
-
-//go:embed manifest.json
-var manifest string
-
-func init() {
-
-	var p protocol.Protocol
-	err := json.Unmarshal([]byte(manifest), &p)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	protocol.Register(&p)
-}
