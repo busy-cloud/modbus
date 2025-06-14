eb1cc8d1b8ab6e6433bbed863d8d881e088280f2
jason <jason@zgwit.com>
2025-06-14 17:53:04 +0800

    合并配置文件

--- internal/modbus.go
+++ internal/modbus.go
@@ -7,3 +7,10 @@ type ModbusConfig struct {
 	Mapper   *Mapper   `json:"mapper,omitempty"`
 	Pollers  []*Poller `json:"pollers,omitempty"`
 }
+
+type Options struct {
+	Tcp             bool  `json:"tcp,omitempty"`              //TCP模式，默认RTU
+	Timeout         int64 `json:"timeout,omitempty"`          //读取超时
+	Polling         bool  `json:"polling,omitempty"`          //开启轮询
+	PollingInterval int64 `json:"polling_interval,omitempty"` //轮询间隔(s)
+}
--- internal/options.go
+++
@@ -1,8 +0,0 @@
-package internal
-
-type Options struct {
-	Tcp             bool  `json:"tcp,omitempty"`              //TCP模式，默认RTU
-	Timeout         int64 `json:"timeout,omitempty"`          //读取超时
-	Polling         bool  `json:"polling,omitempty"`          //开启轮询
-	PollingInterval int64 `json:"polling_interval,omitempty"` //轮询间隔(s)
-}
